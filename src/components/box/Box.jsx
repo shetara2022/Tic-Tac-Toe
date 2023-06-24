1813230b53456b2977032044f30b89c575846eb2
shetara2022 <smithshetarall@gmail.com>
2023-06-24 17:31:27 -0400

    created react app, added body styling, creating box components and styling

---
+++ src/components/box/Box.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './box.css'
+
+export const Box = () => {
+
+    //assign color based on value
+  return (
+      <button className='box'>X</button>
+      
+  )
+}
+
