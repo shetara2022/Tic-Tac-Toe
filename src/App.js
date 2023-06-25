a692ec3cd61add346863a56f879b55122569e72d
shetara2022 <smithshetarall@gmail.com>
2023-06-25 18:26:57 -0400

    handle click on boxes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import './App.css'
+import React, { useState } from 'react';
 import { Board } from './components/Board';
 
 function App() {
-  //list of X's named 'board
-  const board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
+  //create list as state instead of static list. The default value is an Array with 9 elements
+  const [board, setBoard] = useState(Array(9).fill(null));
 
+  //function to handle when box is clicked 
+  const handleBoxClick = (boxIndex) => {
+    const updatedBoard = board.map((value, index) => {
+      if (index === boxIndex) {
+        return "X";
+      } else {
+        return value;
+      }
+    })
+
+    //update board variable
+    setBoard(updatedBoard);
+  }
 
   return (
     <div className="App">
-      <Board board={board} onClick={null} />
+      <Board board={board} onClick={handleBoxClick} />
 
     </div>
   );
