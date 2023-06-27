bb9675039280944b2d38c74fce7adc738656b820
shetara2022 <smithshetarall@gmail.com>
2023-06-26 21:58:02 -0400

    set player x and o on click

--- src/App.js
+++ src/App.js
@@ -6,19 +6,25 @@ function App() {
   //create list as state instead of static list. The default value is an Array with 9 elements
   const [board, setBoard] = useState(Array(9).fill(null));
 
+  //set the player
+  const [xPlayer, setXPlayer] = useState(true)
+
+
   //function to handle when box is clicked 
   const handleBoxClick = (boxIndex) => {
     const updatedBoard = board.map((value, index) => {
       if (index === boxIndex) {
-        return "X";
+        return xPlayer === true ? "X" : "O";
       } else {
         return value;
       }
     })
-
     //update board variable
     setBoard(updatedBoard);
+
+    setXPlayer(!xPlayer);
   }
+
 
   return (
     <div className="App">
